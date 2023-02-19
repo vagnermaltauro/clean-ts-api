ecc0856ce83edb76bdd26a61b15dcb5837785c7b
vagnermaltauro <vagnermaltauro@gmail.com>
2023-02-19 11:10:48 -0300

    refactor: move all procotol imports to a single file

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,6 +1,5 @@
 import { InvalidParamError, MissingParamError } from '../errors'
-import { Controller } from './../protocols/controller'
-import { HttpRequest, HttpResponse } from '../protocols/http'
+import { HttpRequest, HttpResponse, Controller } from '../protocols'
 import { badRequest, serverError } from '../helpers/http-helper'
 import { EmailValidator } from '../protocols/email-validator'
 
